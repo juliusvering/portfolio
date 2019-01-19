0d517fa508bdd0c51ac4cf2d49eee5274a68cd88
Julius <julius.vering@berkeley.edu>
2019-01-18 20:29:59 -0800

    Updates

---
+++ precache-manifest.03786d68c2d7bf6017dd353795f7e3a8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e6eac9f06c6701fdd689ac5c0068200",
+    "url": "/portfolio/static/media/haendlr1.0e6eac9f.png"
+  },
+  {
+    "revision": "292811b97816bcfd3e14",
+    "url": "/portfolio/static/css/main.3a4073f7.chunk.css"
+  },
+  {
+    "revision": "bbd92ccc7e48fdfd42ec",
+    "url": "/portfolio/static/js/1.bbd92ccc.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "51c925c773f6655a72d619945dcf985a",
+    "url": "/portfolio/static/media/personal1.51c925c7.png"
+  },
+  {
+    "revision": "9cd1f66dad896d72d570bc975602ba30",
+    "url": "/portfolio/static/media/personal2.9cd1f66d.png"
+  },
+  {
+    "revision": "292811b97816bcfd3e14",
+    "url": "/portfolio/static/js/main.292811b9.chunk.js"
+  },
+  {
+    "revision": "57f7b66f420bd0d6191688afbcab2cbb",
+    "url": "/portfolio/static/media/haendlr2.57f7b66f.png"
+  },
+  {
+    "revision": "b9648160a34f48d9e7dc39c1faa3d7c7",
+    "url": "/portfolio/static/media/haendlr3.b9648160.png"
+  },
+  {
+    "revision": "e3e926b9fae79d26b105811da60bfa6b",
+    "url": "/portfolio/static/media/screen1.e3e926b9.PNG"
+  },
+  {
+    "revision": "fdf68e7267c5848fbfacd2c3624b0610",
+    "url": "/portfolio/static/media/screen2.fdf68e72.PNG"
+  },
+  {
+    "revision": "05d562d5d98facb01da2431847507eed",
+    "url": "/portfolio/static/media/screen4.05d562d5.PNG"
+  },
+  {
+    "revision": "e3b3b698b0c1977b6dcb2a42d1b1511b",
+    "url": "/portfolio/index.html"
+  }
+];
